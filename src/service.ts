c09b7c9b9fadb86fba04f0d921bfb15a0b5c43c9
Mauko <maukoese@gmail.com>
2025-02-15 10:57:28 +0300

    Create service.ts

---
+++ src/service.ts
@@ -0,0 +1,13 @@
+export default class Service {
+  constructor(string apiKey, string apiSecret) {
+    
+  }
+
+  public function authorize(token = null) {
+    return this;
+  }
+
+  public collect(string provider, string operator, Customer customer) {
+    
+  }
+}
